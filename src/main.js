aa3fb61b7b0d8acde01616f493be19827480af38
Elio Teuber <elioteuber@outlook.com>
2021-08-10 13:20:26 +0200

    vue 2.x option

--- src/main.js
+++ src/main.js
@@ -4,4 +4,14 @@ import router from './router'
 import store from './store'
 import './../node_modules/bulma/css/bulma.css'
 
+// Vue 2.x
+// Vue.use(Vuex);
+// new Vue({
+//   el: '#app',
+//   router,
+//   components: { App },
+//   template: '<App/>',
+//   store: store
+// });
+
 createApp(App).use(store).use(router).mount('#app')
--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,14 @@ import { createStore } from 'vuex'
 import product from './modules/product'
 import cart from './modules/cart'
 
+// Vue 2.x
+// export default new Vuex.Store({
+//   modules: {
+//     product,
+//     cart
+//   }
+// });
+
 export default createStore({
   modules: {
     product,
